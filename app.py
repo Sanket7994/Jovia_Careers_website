bbd5927cf805e5d684982ee88d4363fad9caa3a0
Sanket7994 <109847409+Sanket7994@users.noreply.github.com>
2023-04-13 08:59:15 +0000

    Added login page and optimized job page with application form

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db_via_ID
 
 app = Flask(__name__)
 
@@ -14,6 +14,31 @@ def hello_jovian():
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db_via_ID(id)
+  if not job:
+    return "Not Found", 404
+  else:
+    return render_template('jobpage.html', job=job, company_name="Jovian")
+
+
+@app.route("/api/job/<id>")
+def show_job_json(id):
+  job = load_job_from_db_via_ID(id)
+  return jsonify(job)
+
+
+@app.route("/login")
+def login():
+  return render_template('login.html')
+
+
+@app.route("/signup")
+def signup():
+  return render_template('signup.html')
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -12,9 +12,20 @@ engine = create_engine(db_string,
 
 def load_jobs_from_db():
   with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
+    result = conn.execute(text("SELECT * FROM jobs"))
     jobs = []
     for row in result.all():
       dict_row = row._asdict()
       jobs.append(dict_row)
     return jobs
+
+
+def load_job_from_db_via_ID(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),
+                          {"val": id})
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]._asdict()
